30794c9ede06c6866a9260a02f02dd03ac8b935d
Jhojan Tobón <jhojantobonm@gmail.com>
2023-02-12 12:08:36 -0500

    Reading the DOM

---
+++ 4-Reading-the-DOM/script.js
@@ -0,0 +1,21 @@
+
+const h1 = document.querySelector('h1');
+// const p = document.querySelector('p');
+const p = document.querySelectorAll('p');
+const pClass = document.querySelector('.pClass');
+// const pID = document.querySelector('#pID');
+const pID = document.getElementById('pID');
+const input = document.querySelector('input');
+
+
+console.log(input.value);
+ 
+console.log(
+    {
+        h1,
+        p,
+        pClass,
+        pID,
+        input
+    }
+);
