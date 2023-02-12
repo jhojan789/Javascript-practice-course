8d7a95bfacc333e81dc7f9c578524ebafb7ab76f
Jhojan Tobón <jhojantobonm@gmail.com>
2023-02-12 12:10:04 -0500

    Interacting with user

---
+++ 6-Interacting-with-user/script.js
@@ -0,0 +1,13 @@
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#number1');
+const input2 = document.querySelector('#number2');
+const btnCalculate = document.querySelector('#btnCalculate');
+const result = document.querySelector('.result');
+
+function onClick(){
+    
+    var sum = Number(input1.value) + Number(input2.value);
+
+    console.log(sum);
+    result.innerHTML = 'Result: ' + sum;
+}
